85e1f1929f1ae8cd9a60a6fc97550a449e4166b1
Alan Varela <141166516+AlanDVarela@users.noreply.github.com>
2024-11-30 20:46:38 -0600

    Update Snake.c
    Cambios en el movimiento y en el crecimiento de la serpiente

--- Snake.c
+++ Snake.c
@@ -7,25 +7,29 @@
 #define MAX_SNAKE_LENGTH 20 // Longitud máxima de la serpiente
 
 // Variables globales para la serpiente
-unsigned int snake_length = 2; // Longitud inicial en bloques de 2x2
+unsigned int snake_length = 1; // Longitud inicial en segmentos (cada segmento es 2x2)
 unsigned int snake_x[MAX_SNAKE_LENGTH]; // Coordenadas X de cada segmento
 unsigned int snake_y[MAX_SNAKE_LENGTH]; // Coordenadas Y de cada segmento
+
+// Coordenadas de la manzana
+unsigned int apple_x;
+unsigned int apple_y;
 
 // Funciones
 void set_pixel(unsigned int x, unsigned int y, unsigned int color);
 void create_snake(unsigned int x, unsigned int y);
 void move_snake(unsigned int new_x, unsigned int new_y);
-void create_apple(unsigned int x, unsigned int y);
+void create_apple();
 void grow_snake();
 int check_collision_with_apple(unsigned int head_x, unsigned int head_y, unsigned int apple_x, unsigned int apple_y);
 void reset_game();
 
 void main(void) {
     // Posiciones iniciales
-    unsigned int x = rand() % (MATRIX_WIDTH - 2) + 1; // Coordenada aleatoria x
-    unsigned int y = rand() % (MATRIX_HEIGHT - 2) + 1; // Coordenada aleatoria y
-    unsigned int apple_x = rand() % (MATRIX_WIDTH - 2) + 1; // Coordenada aleatoria manzana
-    unsigned int apple_y = rand() % (MATRIX_HEIGHT - 2) + 1; // Coordenada aleatoria manzana
+    unsigned int x = rand() % (MATRIX_WIDTH / 2) * 2; // Coordenada aleatoria alineada a pasos de 2
+    unsigned int y = rand() % (MATRIX_HEIGHT / 2) * 2; // Coordenada aleatoria alineada a pasos de 2
+    apple_x = rand() % (MATRIX_WIDTH / 2) * 2; // Coordenada aleatoria para la manzana
+    apple_y = rand() % (MATRIX_HEIGHT / 2) * 2; // Coordenada aleatoria para la manzana
 
     // Dirección de memoria del D-PAD y Switch reset
     unsigned int *up = (unsigned int *)D_PAD_0_UP;
@@ -41,7 +45,7 @@ void main(void) {
 
     // Crear serpiente inicial y manzana
     create_snake(x, y);
-    create_apple(apple_x, apple_y);
+    create_apple();
 
     while (1) 
     {
@@ -53,33 +57,28 @@ void main(void) {
         // Verificar entrada del D-PAD
         if (*up == 1 && key_up_pressed == 0) {
             key_up_pressed = 1;
-            if (y < MATRIX_HEIGHT - 2) // Verificar borde superior
-            { 
-                y += 1;
+            if (y < MATRIX_HEIGHT - 2) { // Movimiento hacia arriba
+                y += 2;
                 move_snake(x, y);
             }
         } 
         else if (*down == 1 && key_down_pressed == 0) {
             key_down_pressed = 1;
-            if (y > 0) // Verificar borde inferior
-            { 
-                y -= 1;
+            if (y > 1) { // Movimiento hacia abajo
+                y -= 2;
                 move_snake(x, y);
             }
-        } else if (*left == 1 && key_left_pressed == 0)// Verificar borde izquierdo 
-        {
+        } 
+        else if (*left == 1 && key_left_pressed == 0) {
             key_left_pressed = 1;
-            if (x > 1) 
-            { 
+            if (x > 1) { // Movimiento hacia la izquierda
                 x -= 2;
                 move_snake(x, y);
             }
         } 
-        else if (*right == 1 && key_right_pressed == 0) 
-        {
+        else if (*right == 1 && key_right_pressed == 0) {
             key_right_pressed = 1;
-            if (x < MATRIX_WIDTH - 2) // Verificar borde derecho
-            { 
+            if (x < MATRIX_WIDTH - 2) { // Movimiento hacia la derecha
                 x += 2;
                 move_snake(x, y);
             }
@@ -89,9 +88,7 @@ void main(void) {
         if (check_collision_with_apple(snake_x[0], snake_y[0], apple_x, apple_y)) 
         {
             grow_snake(); // Crecer la serpiente
-            apple_x = rand() % (MATRIX_WIDTH - 2) + 1; // Nueva manzana
-            apple_y = rand() % (MATRIX_HEIGHT - 2) + 1;
-            create_apple(apple_x, apple_y);
+            create_apple(); // Crear nueva manzana
         }
 
         // Liberar teclas
@@ -112,21 +109,15 @@ void set_pixel(unsigned int x, unsigned int y, unsigned int color)
 
 void create_snake(unsigned int x, unsigned int y) 
 {
-    // Inicializar la serpiente
-    snake_length = 2;
+    snake_length = 1; // Longitud inicial de la serpiente
     snake_x[0] = x;
     snake_y[0] = y;
-    snake_x[1] = x - 2; // Segmento inicial detrás de la cabeza
-    snake_y[1] = y;
 
-    // Dibujar la serpiente
-    for (unsigned int i = 0; i < snake_length; i++) 
-    {
-        set_pixel(snake_x[i], snake_y[i], 0x00ff0000);         // Esquina superior izquierda
-        set_pixel(snake_x[i] + 1, snake_y[i], 0x00ff0000);     // Esquina superior derecha
-        set_pixel(snake_x[i], snake_y[i] + 1, 0x00ff0000);     // Esquina inferior izquierda
-        set_pixel(snake_x[i] + 1, snake_y[i] + 1, 0x00ff0000); // Esquina inferior derecha
-    }
+    // Dibujar el segmento inicial (2x2)
+    set_pixel(x, y, 0x00ff0000);
+    set_pixel(x + 1, y, 0x00ff0000);
+    set_pixel(x, y + 1, 0x00ff0000);
+    set_pixel(x + 1, y + 1, 0x00ff0000);
 }
 
 void move_snake(unsigned int new_x, unsigned int new_y) 
@@ -148,23 +139,37 @@ void move_snake(unsigned int new_x, unsigned int new_y)
     snake_x[0] = new_x;
     snake_y[0] = new_y;
 
-    // Dibujar la nueva posición
-    for (unsigned int i = 0; i < snake_length; i++) 
-    {
-        set_pixel(snake_x[i], snake_y[i], 0x00ff0000);
-        set_pixel(snake_x[i] + 1, snake_y[i], 0x00ff0000);
-        set_pixel(snake_x[i], snake_y[i] + 1, 0x00ff0000);
-        set_pixel(snake_x[i] + 1, snake_y[i] + 1, 0x00ff0000);
-    }
+    // Dibujar la nueva posición de la cabeza (2x2)
+    set_pixel(new_x, new_y, 0x00ff0000);
+    set_pixel(new_x + 1, new_y, 0x00ff0000);
+    set_pixel(new_x, new_y + 1, 0x00ff0000);
+    set_pixel(new_x + 1, new_y + 1, 0x00ff0000);
 }
 
-void create_apple(unsigned int x, unsigned int y) 
+void create_apple() 
 {
+    // Generar nueva posición de la manzana
+    int valid_position = 0;
+
+    while (!valid_position) {
+        valid_position = 1;
+        apple_x = rand() % (MATRIX_WIDTH / 2) * 2;
+        apple_y = rand() % (MATRIX_HEIGHT / 2) * 2;
+
+        // Verificar que la manzana no se superponga con la serpiente
+        for (unsigned int i = 0; i < snake_length; i++) {
+            if (check_collision_with_apple(snake_x[i], snake_y[i], apple_x, apple_y)) {
+                valid_position = 0;
+                break;
+            }
+        }
+    }
+
     // Dibujar manzana (2x2 en color verde)
-    set_pixel(x, y, 0x0000ff00);
-    set_pixel(x + 1, y, 0x0000ff00);
-    set_pixel(x, y + 1, 0x0000ff00);
-    set_pixel(x + 1, y + 1, 0x0000ff00);
+    set_pixel(apple_x, apple_y, 0x0000ff00);
+    set_pixel(apple_x + 1, apple_y, 0x0000ff00);
+    set_pixel(apple_x, apple_y + 1, 0x0000ff00);
+    set_pixel(apple_x + 1, apple_y + 1, 0x0000ff00);
 }
 
 void grow_snake() 
@@ -174,31 +179,34 @@ void grow_snake()
         unsigned int new_x = snake_x[snake_length - 1];
         unsigned int new_y = snake_y[snake_length - 1];
 
+        // Ajustar nueva posición del segmento
         if (snake_x[snake_length - 2] < new_x) new_x += 2;
         else if (snake_x[snake_length - 2] > new_x) new_x -= 2;
         else if (snake_y[snake_length - 2] < new_y) new_y += 2;
         else if (snake_y[snake_length - 2] > new_y) new_y -= 2;
 
+        // Agregar nuevo segmento
         snake_x[snake_length] = new_x;
         snake_y[snake_length] = new_y;
         snake_length++;
     }
 }
 
-int check_collision_with_apple(unsigned int head_x, unsigned int head_y, unsigned int apple_x, unsigned int apple_y) {
-    return ((head_x == apple_x && head_y == apple_y) ||
-            (head_x + 1 == apple_x && head_y == apple_y) ||
-            (head_x == apple_x && head_y + 1 == apple_y) ||
-            (head_x + 1 == apple_x && head_y + 1 == apple_y));
+int check_collision_with_apple(unsigned int x, unsigned int y, unsigned int apple_x, unsigned int apple_y) {
+    return ((x == apple_x && y == apple_y) ||
+            (x + 1 == apple_x && y == apple_y) ||
+            (x == apple_x && y + 1 == apple_y) ||
+            (x + 1 == apple_x && y + 1 == apple_y));
 }
 
 void reset_game() {
     for (unsigned int i = 0; i < MATRIX_WIDTH; i++) 
     {
-        for (unsigned int j = 0; j < MATRIX_HEIGHT; j++)
-         {
-            set_pixel(i, j, 0x00000000);//Borrar todos los pixeles(ponerlos en negro)
+        for (unsigned int j = 0; j < MATRIX_HEIGHT; j++) {
+            set_pixel(i, j, 0x00000000); // Borrar toda la matriz
         }
     }
-   main(); // Reiniciar juego
+
+    // Reiniciar la serpiente y la manzana
+    main();
 }
